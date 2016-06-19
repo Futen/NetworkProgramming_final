234726b2e3fbe366d3e1ddcfccf1194a70abbc44
Futen <tdk356@yahoo.com.tw>
2016-06-19 17:47:54 +0800

    gg

--- Log.py
+++ Log.py
@@ -3,10 +3,17 @@ import os
 import numpy as np
 
 F_NAME = 'LogData.npy'
+F_NAME_G = 'LogDataGroup.json'
 if os.path.isfile(F_NAME):
     LogData = list(np.load(F_NAME))
 else:
     LogData = []
+if os.path.isfile(F_NAME_G):
+    f = open(F_NAME_G, 'r')
+    LogDataGroup = json.load(f) 
+    f.close()
+else:
+    LogDataGroup = {}
 
 def NewLogBag():
     a = dict({'who_1':'', 'who_2':'', 'data':[]})
@@ -31,6 +38,10 @@ def FindLog(account1, account2):
     return None
 def SaveLog():
     np.save(F_NAME, LogData)
+def SaveGroupLog():
+    f = open(F_NAME_G, 'w')
+    f.write(json.dumps(LogDataGroup, indent=4))
+    f.close()
 
 def CreateMessage(From, To, Message):
     log = FindLog(From, To)
@@ -44,10 +55,20 @@ def CreateMessage(From, To, Message):
     log['data'].append(Msg)
     SaveLog()
     return True
+def CreateGroupMessage(ID, From, Message):
+    if not ID in LogDataGroup:
+        LogDataGroup[ID] = []
+    data = dict({'from':From, 'message':Message})
+    LogDataGroup[ID].append(data)
+    SaveGroupLog()
+    return True
 def GetMessageData(account1, account2):
     log = FindLog(account1, account2)
     if log is None:
         return []
     else:
         return log['data']
-
+def GetGroupData(ID):
+    if ID in LogDataGroup:
+        return LogDataGroup[ID]
+    return []
